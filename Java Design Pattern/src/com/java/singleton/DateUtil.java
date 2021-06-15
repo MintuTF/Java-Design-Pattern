0bdde979afce938d737de5ffdb800d015e6c786a
mintuta <mintefikr4@gmail.com>
2021-06-14 22:04:51 -0500

    handle multiple thread

--- Java Design Pattern/src/com/java/singleton/DateUtil.java
+++ Java Design Pattern/src/com/java/singleton/DateUtil.java
@@ -2,16 +2,24 @@ package com.java.singleton;
 
 public class DateUtil {
 private static DateUtil instance;
+
+// eager initialization
+static {
+    instance=new DateUtil();
+}
     private DateUtil(){
 
     }
 
 
-    // lazy init
-    public static DateUtil getInstance(){
-        if(instance==null){
-            instance=new DateUtil();
+    // lazy initialization
+    public static synchronized DateUtil getInstance(){
+        synchronized(DateUtil.class){
+            if(instance==null){
+                instance=new DateUtil();
+            }
         }
+
         return instance;
     }
 }
